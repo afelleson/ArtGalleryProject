f371eb39147a64cbe88eaf9d379c134fde6d1fd3
afelleson <86610161+afelleson@users.noreply.github.com>
2023-04-25 11:56:49 -0400

    fix httplib.h include statement path

--- Backend/APIcalls.cpp
+++ Backend/APIcalls.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
-// #include <fstream>
+
 // #include <map>
 // #include <algorithm>
 // #include <cstring>
-#include "httplib.h"
-#include "commentDB.cpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+#include "../httplib.h"
+#include "commentDB.h"
+#include "commentEntry.h"
+
 
 // Call functions from the other C++ files to carry out
 // GET requests and return results in JSON format
@@ -12,21 +18,6 @@
 // (the javascript files will receive the JSON and create
 // basically identitical comment objects to the
 // ones defined in CommentClasses.cpp)
-
-
-string jsonCommentList(vector<commentEntry> pbList) { // produces something that looks like this: {"results":[{"username":"alex","pw":"123"},{"username":"alex2","pw":"1234"}]}
-	string res = "{\"comments\":[";
-	for (int i = 0; i<pbList.size(); i++) {
-		res += pbList[i].jsonify();
-		if (i < pbList.size()-1) {
-			res +=",";
-		}
-	}
-	res += "]}";
-	return res;
-}
-
-
 
 
 // /fetchForArtwork/$artworkID$/sortby/$something$
@@ -52,19 +43,9 @@ string jsonCommentList(vector<commentEntry> pbList) { // produces something that
 // /changerating/$commentID$/$vote$
 
 
-
-
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-
-#include "httplib.h"
-#include "commentDB.h"
-#include "commentEntry.h"
-const int port = 5001;
-
 using namespace std;
+
+const int port = 5001;
 
 ofstream logfile; 
 
