0bca892c1aeb4fee9e49ab0d3e5fee5c2d75276f
Oonicorn <4dylan.evans@gmailcom>
2023-04-26 14:23:23 -0400

    Conflicting change to APIcalls.cpp

--- Backend/APIcalls.cpp
+++ Backend/APIcalls.cpp
@@ -1,16 +1,10 @@
-
+#include <iostream>
+// #include <fstream>
 // #include <map>
 // #include <algorithm>
 // #include <cstring>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-
-#include "../httplib.h"
-#include "commentDB.h"
-#include "commentEntry.h"
-
+#include "httplib.h"
+#include "commentDB.cpp"
 
 // Call functions from the other C++ files to carry out
 // GET requests and return results in JSON format
@@ -18,6 +12,21 @@
 // (the javascript files will receive the JSON and create
 // basically identitical comment objects to the
 // ones defined in CommentClasses.cpp)
+
+
+string jsonCommentList(vector<commentEntry> pbList) { // produces something that looks like this: {"results":[{"username":"alex","pw":"123"},{"username":"alex2","pw":"1234"}]}
+	string res = "{\"comments\":[";
+	for (int i = 0; i<pbList.size(); i++) {
+		res += pbList[i].json();
+		if (i < pbList.size()-1) {
+			res +=",";
+		}
+	}
+	res += "]}";
+	return res;
+}
+
+
 
 
 // /fetchForArtwork/$artworkID$/sortby/$something$
@@ -43,16 +52,26 @@
 // /changerating/$commentID$/$vote$
 
 
-using namespace std;
 
-const int port = 5001;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+#include "httplib.h"
+#include "commentDB.h"
+#include "commentEntry.h"
+const int port = 5004;
+
+using namespace std;
 
 ofstream logfile; 
 
 string jsonResults(vector<commentEntry> pbList) {
 	string res = "{\"results\":[";
 	for (int i = 0; i<pbList.size(); i++) {
-		res += pbList[i].jsonify();
+		res += pbList[i].json();
 		if (i < pbList.size()-1) {
 			res +=",";
 		}
@@ -83,23 +102,63 @@ int main() {
     	res.status = 200;
   	});
   	  	
+  	// svr.Get(R"(/comment/find/(.*))", [&](const httplib::Request& req, httplib::Response& res) {
+    // 	res.set_header("Access-Control-Allow-Origin","*");
+
+    // 	string last = req.matches[1];
+    // 	results = ctdb.find(last);
+    // 	string json = jsonResults(results);
+    // 	res.set_content(json, "text/json");
+    // 	res.status = 200;
+  	// });
+  	
+  	// svr.Get(R"(/comment/last/(.*))", [&](const httplib::Request& req, httplib::Response& res) {
+    // 	res.set_header("Access-Control-Allow-Origin","*");
+
+    // 	string last = req.matches[1];
+    // 	results = ctdb.findByLast(last);
+    // 	string json = jsonResults(results);
+    // 	cout << "Last: " << json << endl;
+    // 	res.set_content(json, "text/json");
+    // 	res.status = 200;
+  	// });
+  	
+  	// svr.Get(R"(/comment/first/(.*))", [&](const httplib::Request& req, httplib::Response& res) {
+    // 	res.set_header("Access-Control-Allow-Origin","*");
+
+    // 	string first = req.matches[1];
+    // 	results = ctdb.findByFirst(first);
+    // 	string json = jsonResults(results);
+    // 	cout << "first: " << json << endl;
+    // 	res.set_content(json, "text/json");
+    // 	res.status = 200;
+  	// });  	
+
+  	// svr.Get(R"(/comment/type/(.*))", [&](const httplib::Request& req, httplib::Response& res) {
+    // 	res.set_header("Access-Control-Allow-Origin","*");
+
+    // 	string type = req.matches[1];
+    // 	results = ctdb.findByType(type);
+    // 	string json = jsonResults(results);
+    // 	cout << "type: " << json << endl;
+    // 	res.set_content(json, "text/json");
+    // 	res.status = 200;
+  	// });  
   	
   	svr.Get(R"(/comment/add/(.*)/(.*)/(.*)/(.*)/(.*)/(.*))", [&](const httplib::Request& req, httplib::Response& res) {
     	res.set_header("Access-Control-Allow-Origin","*");
 
     	string name = req.matches[1];
     	string body = req.matches[2];
-		string artworkID = req.matches[3];
-    	string x = req.matches[4];
-    	string y = req.matches[5];
-		string width = req.matches[6];
-		// string rating = req.matches[7];
-		// string isPinned = req.matches[8];
-    	cdb.addComment(name,body,artworkID,x,y,width,0,0); // 0 for initial rating and isPinned status
+    	string x = req.matches[3];
+    	string y = req.matches[4];
+		string width = req.matches[5];
+		string rating = req.matches[6];
+    	cdb.addEntry(name,body,x,y,zoom,score);
 
     	res.set_content("{\"status\":\"success\"}", "text/json");
     	res.status = 200;
-  	});
+  	}); 	
  
    	// svr.Get(R"(/comment/update/(.*)/(.*)/(.*)/(.*)/(.*))", [&](const httplib::Request& req, httplib::Response& res) {
     // 	res.set_header("Access-Control-Allow-Origin","*");
