53f7f9fbb64978bd2f06d6b42f3d197143ba6995
Oonicorn <4dylan.evans@gmailcom>
2023-05-02 14:09:55 -0400

    Cleaning out old/unused code/comments

--- Pages/listView/listView.js
+++ Pages/listView/listView.js
@@ -1,5 +1,4 @@
 var commentList = [];
-// const baseUrl = 'C:\Users\4dyla\Programming\ArtGalleryProject\Pages\listView';
 var baseUrl = 'http://18.218.64.106:5001';
 
 var oldJson = "";
@@ -10,8 +9,6 @@ fetchArtwork(6);
 
 window.addEventListener('beforeunload', function (event) {
     clearInterval(fetchRegularly); // stop updating member list
-    // event.preventDefault() // these two lines cause a pop-up asking the user if they really want to close the tab
-    // event.returnValue = '' // these two lines cause a pop-up asking the user if they really want to close the tab
   });
 
 document.getElementById('submit-comment').addEventListener("click", (e)=> {
@@ -19,24 +16,6 @@ document.getElementById('submit-comment').addEventListener("click", (e)=> {
 	addComment();
 });
 
-/* Set up events */
-// $(document).ready(function() {
-	// Add a click event for the filter button
-	// document.querySelector("#filter-btn").addEventListener("click", (e) => {
-		// var searchStr = 	document.querySelector("#search").value;
-    	// findMatches(searchStr);
-	// });
-
-    // $(".dropdown-menu li a").click(function() {
-        // var selection = $(this).text();
-        // $(this).parents(".btn-group").find('.btn').html(selection + ' <span class="caret"></span>');
-    // });
-
-	// findMatches(" ");
-
-// });
-
-// Artwork Database functions
 
 function displayArtInfo(results) {
     artworkList = results["result"];
@@ -88,9 +67,6 @@ function completeDownvote(results){
     };
 }
 
-// upvoteColor = "green";
-// downvoteColor = "red";
-// noVoteColor = "gray";
 
 function incrementRating(commentID){
     fetch(baseUrl + '/comment/changerating/' + commentID + "/" + "1", {
@@ -126,41 +102,6 @@ function toggleUpButton(buttonPressed, commentID){
       }
 }
 
-    // OLD TOGGLEUPBUTTON CODE
-    // // if green, turn gray (could do by running reset on the upvote button)
-    // if (buttonPressed.style.backgroundColor == upvoteColor) {
-    //     buttonPressed.style.backgroundColor = noVoteColor;
-        
-    //     // decrement vote
-    //     fetch(baseUrl + '/comment/changerating/' + commentID + "/" + "-1", {
-    //         method: 'get'
-    //     })
-    //     .then(response => response.json())
-    //     .then(json => completeUpvote(json))
-    //     .catch(error => {
-    //         {
-    //             alert("Upvote Error: Something went wrong: " + error);
-    //         }
-    //     })
-    // }
-    // // if gray, turn green
-    // else if (buttonPressed.style.backgroundColor == noVoteColor) {
-    //     buttonPressed.style.backgroundColor = upvoteColor;
-
-    //     // increment vote
-    //     fetch(baseUrl + '/comment/changerating/' + commentID + "/" + "1", {
-    //         method: 'get'
-    //     })
-    //     .then(response => response.json())
-    //     .then(json => completeUpvote(json))
-    //     .catch(error => {
-    //         {
-    //             alert("Upvote Error: Something went wrong: " + error);
-    //         }
-    //     })
-    // } else {
-    //     console.log("Unexpected upvote button color (" + buttonPressed.style.backgroundColor + "); no vote cast.");
-    // }
 
 function resetVoteButton(buttonToActOn){
     // turn it gray
@@ -176,73 +117,26 @@ function toggleDownButton(buttonPressed, commentID){
       }
 }
 
-// function toggleDownButton(buttonPressed, commentID){
-//     // if red, turn gray (could do by running reset on the upvote button)
-//     if (buttonPressed.style.backgroundColor == downvoteColor) {
-//         buttonPressed.style.backgroundColor = noVoteColor;
-
-//         // increment vote
-//         fetch(baseUrl + '/comment/changerating/' + commentID + "/" + "1", {
-//             method: 'get'
-//         })
-//         .then(response => response.json())
-//         .then(json => completeUpvote(json))
-//         .catch(error => {
-//             {
-//                 alert("Downvote Error: Something went wrong: " + error);
-//             }
-//         })
-//     }
-//     // if gray, turn red
-//     else if (buttonPressed.style.backgroundColor == noVoteColor) {
-//         buttonPressed.style.backgroundColor = downvoteColor;
-
-//         // decrement vote
-//         fetch(baseUrl + '/comment/changerating/' + commentID + "/" + "-1", {
-//             method: 'get'
-//         })
-//         .then(response => response.json())
-//         .then(json => completeDownvote(json))
-//         .catch(error => {
-//             {
-//                 alert("Downvote Error: Something went wrong: " + error);
-//             }
-//         })
-//     } else {
-//         console.log("Unexpected upvote button color (" + buttonPressed.style.backgroundColor + "); no vote cast.");
-//     }
-// }
 
 function upvote(buttonPressed, commentID){
-    // buttonPressed.classList.toggle('active');
     console.log("upvoting comment with ID = " + commentID);
-
     // turn button green or gray, whichever one it wasn't before
     toggleUpButton(buttonPressed, commentID);
     // reset downvote button, which has an element id based on the ID of the comment these buttons apply to
     resetVoteButton(document.getElementById("downvote-" + commentID));
-    
-
-    // start timer for this comment (write over old timer)
 }
 
 function downvote(buttonPressed, commentID){
-    // buttonPressed.classList.toggle('active');
     console.log("downvoting comment with ID = " + commentID);
-
     // turn button green or gray, whichever one it wasn't before
     toggleDownButton(buttonPressed, commentID);
     // reset downvote button, which has an element id based on the ID of the comment these buttons apply to
     resetVoteButton(document.getElementById("upvote-" + commentID));
-    
-
-    // start timer for this comment (write over old timer)
 }
 
 function displayComments(isJsonDiff, results) {
     if (isJsonDiff){
         commentList = results["results"];
-        // console.log("Results:"+JSON.stringify(commentList));
         console.log("remaking table of comments");
         document.getElementById("searchresults").innerHTML = formatComments(commentList);
     }
@@ -250,26 +144,18 @@ function displayComments(isJsonDiff, results) {
 
 function isJsonDifferent(newJson){
     if (JSON.stringify(oldJson)===JSON.stringify(newJson)){
-        // console.log("json not different");
         return false;
     } else {
-        // console.log("Old JSON:"+JSON.stringify(newJson["results"]));
-        // console.log("NEW JSON:"+JSON.stringify(oldJson["results"]));
         oldJson = newJson; // update saved json
-        // console.log("json is different");
         return true;
     }
 }
 
 function fetchCommentsForArtwork() {
-    // artworkID = $('#artworkID').val();
-    // sortParam = $('#sortparam').val()
 	// Temp Values
     var artworkID = "0";
     var sortParam = "ID";
 	
-    // console.log("Attempting to fetch artwork comments");
-    // console.log("artworkID: " + artworkID + ", sort param: " + sortParam);
     fetch(baseUrl + '/comment/fetchforwork/' + artworkID + "/" + sortParam, {
             method: 'get'
         })
@@ -287,8 +173,6 @@ function processAdd(results) {
     console.log("Add:", results["status"]);
     document.getElementById("addname").value = "";
     document.getElementById("addbody").value = "";
-
-    // findMatches(" ");
 
 }
 
@@ -314,45 +198,5 @@ function addComment() {
 }
 
 
-// NOT NEEDED FOR COMMENTS?
-// function updateContact() {
-
-	//Get ID in the modal
-	// var modal = document.querySelector("#editContact");
-	// id = modal.getAttribute("editid");
-	
-    // console.log("Attempting to edit an entry:"+id); 
-
-    // fetch(baseUrl + '/contact/update/' + id + '/' + document.getElementById("editfirst").value 
-    		// + '/' + document.getElementById("editlast").value + '/' + document.getElementById("editphone").value + '/' + document.getElementById("edittype").innerText, {
-                // method: 'get'
-            // })
-        // .then(alert("Record for " + document.getElementById("editfirst").value + ' ' + document.getElementById("editlast").value + " updated"))
-        // .catch(error => {
-            // {
-                // alert("Edit Error: something went wrong:" + error);
-            // }
-        // });
-        
-    // findMatches(" ");
-
-// }
-
-// SAVE FOR MODERATION PAGE
-// function deleteContact(id) {
-
-    // console.log("Attempting to delete an entry:" + id);
-    // fetch(baseUrl + '/contact/delete/' + id, {
-            // method: 'get'
-        // })
-        // .then(alert("Deleted Record: " + id))
-        // .catch(error => {
-            // {
-                // alert("Delete Error: Something went wrong:" + error);
-            // }
-        // });
-     // findMatches(" ");
-
-// }
 
 
