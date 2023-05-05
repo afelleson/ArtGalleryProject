1c5015d09bdccb2095b2bcd5424862b593b0f84b
Oonicorn <4dylan.evans@gmailcom>
2023-05-05 14:30:04 -0400

    fix error name for fetch art info

--- Pages/listView/listView.js
+++ Pages/listView/listView.js
@@ -44,7 +44,7 @@ function fetchArtList() {
     .then(json => displayArtList(json))
     .catch(error => {
         {
-            alert("Fetch Art Error: Something went wrong: " + error);
+            alert("Fetch Art Info Error: Something went wrong: " + error);
         }
     })
 }
