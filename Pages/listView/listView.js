c791c143194106de1e8699b8025530141863c836
Oonicorn <4dylan.evans@gmailcom>
2023-05-06 18:23:33 -0400

    add dropdown to navbar instead of replacing navbar content

--- Pages/listView/listView.js
+++ Pages/listView/listView.js
@@ -51,7 +51,7 @@ function fetchArtList() {
 
 function displayArtList(results) {
     artList = results["results"];
-    document.getElementById("navbarSupportedContent").innerHTML = formatNavDropdown(artList);
+    document.getElementById("navbarSupportedContent").innerHTML += formatNavDropdown(artList);
 }
 
 function fetchArtwork(artworkID) {
