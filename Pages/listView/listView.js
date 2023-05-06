8188db8da1fa3c37e126038a63f724022fd74c8a
Oonicorn <4dylan.evans@gmailcom>
2023-05-06 18:49:08 -0400

    added console log for dropdown menu button generation

--- Pages/listView/listView.js
+++ Pages/listView/listView.js
@@ -88,10 +88,11 @@ function formatNavDropdown(json) {
     var result = "<div class='dropdown'><button class='btn btn-secondary dropdown-toggle' type='button' data-bs-toggle='dropdown' aria-expanded='false'>";
     result +=  "Artwork";
     result += "</button><ul class='dropdown-menu'>";
-    json.forEach(function(entry, i){
+    json.forEach(function(entry, i) {
+        console.log("Generating button for artwork");
         result += "<li><button class='dropdown-item' onClick='changeArtwork(";
         result += entry["ID"];
-        result += ")>"
+        result += ")>";
         result += entry["title"];
         result += "</button></li>";
     })
