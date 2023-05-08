820dd3dd44720cfeb45cbc7a83fd853d169539c7
afelleson <86610161+afelleson@users.noreply.github.com>
2023-05-07 21:08:14 -0400

    reorganize backend functions and add comments

--- Backend/commentEntry.cpp
+++ Backend/commentEntry.cpp
@@ -1,22 +1,10 @@
-// Defines the comment class in C++
-// Maybe the artwork class too
-
-
-// Defines functions that interact with the database:
-
-// commentEntryList.addcommentEntry()
-// queryAllComments(string sortBy) : commentEntryList
-// queryWorkComments(string sortBy) : commentEntryList
-
-// comment.EditRating()
-
-// commentEntryList.jsonify()
+// This program defines the commentEntry class, which stores all the information about a comment pulled from the database
 
 #include <iostream>
 #include "commentEntry.h"
 
 commentEntry::commentEntry(){
-    id = ""; // Q: what to do here?
+    id = NULL; //(?)
     name = "";
     text = "";
     artworkID = "";
@@ -38,17 +26,17 @@ commentEntry::commentEntry(sql::SQLString id_input, sql::SQLString name_input, s
     x = "0";
     y = "0";
     width = "0";
-    rating = rating_input; // potential default to add later: 0
-    isPinned = isPinned_input;  // potential default to add later: 0
+    rating = rating_input; // potential default (currently handled in APIcalls.cpp): 0
+    isPinned = isPinned_input;  // potential default (currently handled in APIcalls.cpp): 0
 }
 
 commentEntry::commentEntry(sql::SQLString id_input, sql::SQLString name_input, sql::SQLString text_input, sql::SQLString artworkID_input, sql::SQLString x_input,sql::SQLString y_input, sql::SQLString width_input, sql::SQLString rating_input,sql::SQLString isPinned_input){
     id = id_input;
     name = name_input;
     text = text_input;
-    if (text_input==""){
-        cerr << "Warning: no comment text provided" << endl;
-    }
+    // if (text_input==""){
+    //     cerr << "Warning: no comment text provided" << endl;
+    // }
     artworkID = artworkID_input;
     x = x_input;
     y = y_input;
@@ -58,22 +46,6 @@ commentEntry::commentEntry(sql::SQLString id_input, sql::SQLString name_input, s
     // }
     rating = rating_input;
     isPinned = isPinned_input;
-
-    // TODO: use this template to type-check inputs before/while converting them to the right type.
-    // Alternatively (and better), do not have the inputs be SQLStrings
-    // We don't want this to actually fail tho. Or do we?
-    // try {
-    //     userParamType = stoi(userParamTypeString); // this will throw an invalid_argument if userParamTypeString is not easy to convert to an int
-    //     if ((userParamType > 3) || (userParamType < 1)){
-    //       throw invalid_argument("Input too large.");
-    //     } else {
-    //       keepgoing  = 1;
-    //     }
-    //   }
-    //   catch (invalid_argument) {
-    //     cout << "Please type 1, 2, or 3.\n";
-    //     cin >> userParamTypeString;
-    //   }
 }
 
 bool commentEntry::containsSelection(string commentID){
